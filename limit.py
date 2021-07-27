d1ad59b8b25cd966c8a725268f614cf86590f73f
Steve Cline <steve.cline@weldre4.org>
2021-07-27 17:26:56 -0600

    Create limit.py

---
+++ limit.py
@@ -0,0 +1,16 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+button = 4
+
+GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+
+while True:
+    button_state = GPIO.input(button)
+    if button_state == GPIO.HIGH:
+      print ("HIGH")
+    else:
+      print ("LOW")
+    time.sleep(0.5)
